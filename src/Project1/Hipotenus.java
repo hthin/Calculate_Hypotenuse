d4488da1dbbbeaa04dc3c8e02f68fbf826e9e2b1
Hüseyin <hince1992@gmail.com>
2023-09-20 21:12:45 +0200

    Initial commit

---
+++ src/Project1/Hipotenus.java
@@ -0,0 +1,23 @@
+package Project1;
+
+import java.util.Scanner;
+
+public class Hipotenus {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1. Kenar : ");
+        int k1 = scan.nextInt();
+
+        System.out.print("2. Kenar : ");
+        int k2 = scan.nextInt();
+
+        double k3 = Math.sqrt( (k1 * k1) + (k2 * k2));
+
+        System.out.println("Hipotenüs = " + (int) k3);
+
+
+
+    }
+
+}
